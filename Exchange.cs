87b37a681551bf241728344b1f5fd88bd519111d
s.zorin <s.zorin@logexpert.ru>
2018-03-04 12:13:58 +0300

    add method mileage

--- Exchange.cs
+++ Exchange.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace LogExpertSharp
 {
@@ -33,6 +35,32 @@ namespace LogExpertSharp
                     : throw new Exception($"incorrect server value: {result}");
             }
         }
-    }
 
+        public async Task<float> GetMileage(int objectId, 
+            DateTime begin, DateTime end, bool useRideDetector = false, bool useAltitude = true)
+        {
+            var format = "dd/MM/yyyy HH:mm:ss.fff";
+
+            var i = ( nameof(objectId) , objectId.ToString() );
+            var b = ( nameof(begin), begin.ToString(format) );
+            var e = ( nameof(end), end.ToString(format) );
+            var rd = ( nameof(useRideDetector), useRideDetector.ToString() );
+            var alt = ( nameof(useAltitude), useAltitude.ToString() );
+
+            var method = $"{nameof(Exchange)}/{nameof(GetMileage)}";
+            var task = Connection.Post(method, i, b, e, rd, alt);
+
+            using(var response = ( await task ).EnsureSuccessStatusCode() )
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<MileageItem[]>(result)
+                    .Select(x => x.Length).Sum();
+            }
+        }
+
+        private sealed class MileageItem
+        {
+            public float Length;
+        }
+    }
 }
