85ea5281b02f0bc33a1315706b9bb6c7373a2451
s.zorin <s.zorin@logexpert.ru>
2018-03-06 09:52:35 +0300

    modify model

--- Alerts/Alert.cs
+++ Alerts/Alert.cs
@@ -1,26 +1,31 @@
 using System;
 using Newtonsoft.Json;
 
+/*
+
+"Id": 146810,
+"TriggerId": 63,
+"ObjectId": 568,
+"CreationDate": "2018-03-05T15:50:00.68",
+"MessageText": "Объект МАЗ О214МК потерял связь или координаты!",
+"SentTo": "Не указан адресат",
+"AlertsViews": [],
+"TriggerType": "Потеря координат"
+
+ */
+
 namespace LogExpertSharp.Alerts
 {
     public sealed class Alert
     {
-        [JsonProperty("id")]
         public int Id;
 
-        [JsonProperty("text")]        
-        public string Text;
-
-        [JsonProperty("objectId")]
+        [JsonProperty("MessageText")]        
+        public string Message;
+        public string SentTo;
+        public string TriggerType;
         public int ObjectId;
 
-        [JsonProperty("objectName")]
-        public string ObjectName;
-
-        [JsonProperty("objectType")]
-        public string ObjectType;
-
-        [JsonProperty("creationDate")]
         [JsonConverter(typeof(CustomDateTimeConverter))]
         public DateTime CreationDate;
     }
